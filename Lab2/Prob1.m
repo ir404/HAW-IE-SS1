48ce75a997254d2fd2b62ea5d25e13318dd00010
Imran <imranrizwan1115@gmail.com>
2025-11-20 12:33:12 +0100

    Lab 2 Stuff

--- Lab2/Prob1.m
+++ Lab2/Prob1.m
@@ -1,68 +1,87 @@
-% 19 Nov 2025
-% SS1 Lab 2 - Problem 1
-% Author: Imran Rizwan
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 19 Nov 2025                                                                           %
+% SS1 Lab 2 - Problem 1                                                                 %
+% Decomposes a signal to its even and odd parts                                         %
+% Authors: Imran Rizwan 2778020, Lawrence Byabato 2778215, Gibriel Zibo 2778062         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear();
 clc();
 
+% A function to split a given function x to its even and odd parts
 function [x_even, x_odd] = split_even_odd_parts(x)
     N = length(x);
     mid = (N + 1) / 2;                                  % mid index corresponds to t=0
 
-    % initialise vectors to hold the even and odd values of vector x
-    x_even = zeros(size(x));
-    x_odd = zeros(size(x));
+    if (mod(N, 2) == 0)
+        error("x should contain an odd length");
+    else 
+        % initialise vectors to hold the even and odd values of vector x
+        x_even = zeros(size(x));
+        x_odd = zeros(size(x));
+    
+        for k = 0 : (mid-1)
+            t = mid + k;                % t  index
+            neg_t = mid - k;            % -t index
+            
+            x_pos = x(t);                        
+            x_neg = x(neg_t);
+            
+            x_even(t) = 0.5 * (x_pos + x_neg);
+            x_even(neg_t) = x_even(t);
 
-    for k = 0 : (mid-1)
-        t = mid + k;
-        neg_t = mid - k;
-        x_pos = x(t);                        
-        x_neg = x(neg_t);
-        x_even(t) = 0.5 * (x_pos + x_neg);
-        x_even(neg_t) = x_even(t);
-        x_odd(t) = 0.5 * (x_pos - x_neg);
-        x_odd(neg_t) = -x_odd(t);
+            x_odd(t) = 0.5 * (x_pos - x_neg);
+            x_odd(neg_t) = -x_odd(t);
+        end
     end
 end
 
-x = [-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7];
+
+% ---------------------------------- Test Program ------------------------------------- 
+
+% test funcion and relevant properties
+N = 15;
+mid = (N - 1) / 2;
+t = -N : N;               % corresponds to the time values in the horizontal axis
+x = zeros();
+x(t == 0) = 0.5;
+x(t > 0) = 1;
+
 [x_even, x_odd] = split_even_odd_parts(x);
 sum_even_odd = x_even + x_odd;
 
-% Display the results
-disp('Even Part:');
-disp(x_even);
-disp('Odd Part:');
-disp(x_odd);
-totalSum = sum(x);
-disp('Sum of even and odd parts:');
-disp(sum_even_odd);
 
-% Plot x, x_even, x_odd and sum_even_odd in subplots
 figure;
 
-subplot(4, 1, 1);
-plot(x, 'b-o');
+subplot(4,1,1)
+stem(t, x, 'r-o')
+axis([min(t) max(t) min(x) max(x)])
+set(gca, 'XTick', t);
 title('Original Signal');
-xlabel('Index');
-ylabel('Amplitude');
+ylabel('x(t)');
+xlabel('t');
 
-subplot(4, 1, 2);
-plot(x_even, 'g-o');
+subplot(4,1,2)
+stem(t, x_even, 'g-o')
+% axis([min(t) max(t) min(x_even) max(x_even)]);
+set(gca, 'XTick', t);
 title('Even Part');
-xlabel('Index');
-ylabel('Amplitude');
+ylabel('x_e(t)');
+xlabel('t');
 
-subplot(4, 1, 3);
-plot(x_odd, 'r-o');
+subplot(4,1,3)
+stem(t, x_odd, 'b-o')
+axis([min(t)-1 max(t)+1 min(x_odd) max(x_odd)])
+set(gca, 'XTick', t);
 title('Odd Part');
-xlabel('Index');
-ylabel('Amplitude');
+ylabel('x_o(t)');
+xlabel('t');
 
-subplot(4, 1, 4);
-plot(sum_even_odd, 'y-o');
-title('Sum of Even and Odd Parts');
-xlabel('Index');
-ylabel('Amplitude');
-
+subplot(4,1,4)
+stem(t, sum_even_odd, 'y-o')
+axis([min(t) max(t) min(sum_even_odd) max(sum_even_odd)])
+set(gca, 'XTick', t);
+title('Sum of Even + Odd')
+ylabel('x_e(t) + x_o(t)');
+xlabel('t');
 
--- Lab2/Prob2.m
+++ Lab2/Prob2.m
@@ -1,31 +1,46 @@
-% 19 Nov 2025
-% SS1 Lab 2 - Problem 1
-% Author: Imran Rizwan
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 19 Nov 2025                                                                           %
+% SS1 Lab 2 - Problem 2                                                                 %
+% Compute the Fourier series representation of a square waveform by                     %
+% calculating the coefficients and summing them                                         %
+% Authors: Imran Rizwan 2778020, Lawrence Byabato 2778215, Gibriel Zibo 2778062         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear();
 clc();
 
 % Parameters
-N = 40;                                 % Number of Fourier series coefficients (k = 0,1,2,...,N)
-f0 = 1;                                 % Fundamental frequency of the square wave [in Hz]
+N = 200;                                % Number of Fourier series coefficients (k = 0,1,2,...,N)
+f0 = 2;                                 % Fundamental frequency of the square wave [in Hz]
 T0 = 1/f0;                              % Period
-t = linspace(-2*T0, 2*T0);              % Vector defining time steps over 4 periods
+t = 0 : 0.0005 : 2*T0;                  % time vector over 2 periods
 
 % define the square function piecewise
 f = @(t) (t >= 0 & t < T0/2).*1 + (t >= T0/2 & t < T0).*(-1);
 
 % Compute Fourier coefficient a_k for k = 0,1,2,3,...,N
-coefficent_a = zeros(1, N);
-for k = 1 : N 
-    coefficent_a(k) = (2/T0) * integral(@(t) f(t).* cos(k * (2*pi*f0) * t), 0, T0);
-end
+coefficent_a = zeros(1, N);             % all zero as the square wave given in the task is odd
 
 % Compute Fourier coefficient b_k for k = 1,2,...,N
 coefficient_b = zeros(1, N); 
-for k = 2 : N
-    coefficient_b(k) = (2/T0) * integral(@(t) f(t).* sin(k * (2*pi*f0) * t), 0, T0);
+for k = 1 : N
+    if (mod(k, 2) ~= 0)
+        coefficient_b(k) = 4 / (pi * k);
+    end
 end
 
-% Compute the Fourier series representation of the square wave
+% Compute the Fourier series representation by adding each harmonic 
 fourierSeries = zeros(size(t));
+for k = 1 : N
+    harmonic_k = coefficient_b(k) * sin(2*pi*f0 * k * t);       % a_k not added as its zero
+    fourierSeries = fourierSeries + harmonic_k;
+end             % cutoff freq is N * f0
 
+
+figure
+
+plot(t, fourierSeries);
+titleStr = ['Fourier Series of a Square Wave | N = ', num2str(N), ', f0 = ' num2str(f0), ' Hz'];
+title(titleStr);
+ylabel('x(t)');
+xlabel('t (s)');
